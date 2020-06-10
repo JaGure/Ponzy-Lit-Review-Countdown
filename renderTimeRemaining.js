70b99ce8b340af229116d98ee98544f9e7ea5694
jagure <jake.gurevitch@gmail.com>
2020-06-10 18:32:19 -0400

    Just cleaned up the code a lil

--- renderTimeRemaining.js
+++ renderTimeRemaining.js
@@ -3,7 +3,7 @@ const computeTimeRemaining = require('./timeRemaining')
 const updateTimeRemaining = () => {
     const { days, hours, minutes, seconds } = computeTimeRemaining()
 
-    const timeText = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ' 
+    const timeText = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's' 
 
     document.getElementById('timer-text').innerHTML = timeText;
 }
